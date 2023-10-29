330437e569e0b1861ef69b60ab0a1665a740da98
IBaronap <isabella.baronap@gmail.com>
2023-10-29 18:34:02 -0500

    Merged

--- Fractal/src/App.jsx
+++ Fractal/src/App.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-<<<<<<< HEAD
-import { AppRouter } from './AppRouter'
-=======
 import { TeamInteraction } from './components/team-interaction/TeamInteraction'
->>>>>>> a92c1b07a36cc500b970a3581440732bb7f5bee1
 
 function App () {
   return (
     <>
-<<<<<<< HEAD
-      <AppRouter />
-=======
       <TeamInteraction />
->>>>>>> a92c1b07a36cc500b970a3581440732bb7f5bee1
     </>
   )
 }
