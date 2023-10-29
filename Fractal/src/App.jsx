a92c1b07a36cc500b970a3581440732bb7f5bee1
valenarangot <valentina.arango.tenorio@gmail.com>
2023-10-29 18:23:09 -0500

    main actualized

---
+++ Fractal/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TeamInteraction } from './components/team-interaction/TeamInteraction'
+
+function App () {
+  return (
+    <>
+      <TeamInteraction />
+    </>
+  )
+}
+
+export default App
---
+++ Fractal/src/data/membersData.js
@@ -0,0 +1,13 @@
+export const membersData = [
+    {
+        id: 1,
+        name: 'Valentina Arango',
+        charge: 'UX/UI designer',
+        description: 'Reallu cool',
+        image: 'assets/team-members/team-val.png',
+        tools: [
+            { name: 'Figma', icon: '../../assets/tools/figma.png'},
+            { name: 'Adobe', icon: '../../assets/tools/adobe.png'},
+        ],
+    },
+];
