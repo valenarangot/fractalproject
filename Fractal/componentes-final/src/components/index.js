2c346ca10ccd43584d35768c55a90762ad3c0fe1
Jcdorado <88757034+Jcdorado@users.noreply.github.com>
2023-10-30 01:01:59 -0500

    Add files via upload

---
+++ Fractal/componentes-final/src/components/index.js
@@ -0,0 +1,6 @@
+export * from "./button-back/ButtonBack"
+export * from "./button-cancel/ButtonCancel"
+export * from "./card-1/Card1"
+export * from "./card-2/Card2"
+export * from "./card-3/Card3"
+export * from "./proyect-card/ProyectCard"
---
+++ Fractal/componentes-final/src/data/ProjectData.js
@@ -0,0 +1,91 @@
+export const ProjectData = [
+    {
+        id: 1,
+        title:"Nintendo OOH Experience",
+        owner:"Andrés Narvaez, Isabella Barona",
+        type:"UI-FrontEnd",
+        description:"En este protecto se busca crear un prototipo interactivo de una experiencia Out-Of-Home. ",
+        image:"./assets/project-img/Nintendo.png", 
+    },
+
+    { 
+        id: 2,
+        title:"Tuner",
+        owner:"Valentina Arango",
+        type:"UI-Ux",
+        description:"Es un espacio social donde puedes compartir tus canciones favoritas con tus amigos.",
+        image:"./assets/project-img/Tuner.png", 
+    },
+
+    {
+        id: 3,
+        title:"Teep",
+        owner:"Andrés Narvaez",
+        type:"UI-UX",
+        description:"Un proyecto que busca mejorar las experiencias de aquellas personas que están aprendiendo a programar.",
+        image:"./assets/project-img/Teep.png", 
+    },
+
+    {
+        id: 4,
+        title:"Mauritius",
+        owner:"Isabella Barona",
+        type:"Branding e identidad visual",
+        description:"En este proyecto se realizó el diseño de marca para el país del pcéano índico, Mauricio. ",
+        image:"./assets/project-img/Mauritius.png", 
+    },
+
+    {
+        id: 5,
+        title:"FitMate",
+        owner:"Isabella Barona",
+        type:"UX-UI",
+        description:"Este producto digital motiva a las personas a establecer y cumplir sus metas. ",
+        image:"./assets/project-img/FitMate.png", 
+    },
+
+    {
+        id: 6,
+        title:"Ateon",
+        owner:"Andrés Narvaez",
+        type:"UX-UI",
+        description:"En este proyecto buscamos crear un sistema visual capaz de darle solución a las complicaciones del ususario en la educación virtual.",
+        image:"./assets/project-img/Ateon.png", 
+    },
+
+    {
+        id: 7,
+        title:"Multiskilled for you",
+        owner:"Valentina Arango",
+        type:"UX-UI",
+        description:"Esta plataforma busca sacar provecho de la digitalización para ayudar y mantener enfocadas a las personas durante sus sesiones de trabajo.",
+        image:"./assets/project-img/Multiskilled.png", 
+    },
+
+    {
+        id: 8,
+        title:"OOH Mercedes-Benz",
+        owner:"Juan Camilo Dorado",
+        type:"UI-FrontEnd",
+        description:"Buscamos idear, diseñar y programar un prototipo para la experiencia Out-Of-Home con la marca Mercedes-Benz.",
+        image:"./assets/project-img/OOH-Mercedes_Benz.png", 
+    },
+
+    {
+        id: 9,
+        title:"Flexin",
+        owner:"Juan Camilo Dorado",
+        type:"Ux-UI",
+        description:"Esta aplicación esta dedicada para la venta de calzado deportivo en línea.",
+        image:"./assets/project-img/Flexin.png", 
+    },
+
+    {
+        id: 10,
+        title:"Behance for chefs",
+        owner:"Valentina Atango",
+        type:"UX-UI",
+        description:"Rediseño de la plataforma Behance modificando el logo y el diseño web para todos aquellos apasionados por la cocina.",
+        image:"./assets/project-img/Behance-para-chefs.png", 
+    },
+]
